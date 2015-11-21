d56110a1d5ddf101217caa75c3796186de8031ef
Jackie Huynh <jackiehuynh@Jackies-MacBook-Pro.local>
2015-11-21 14:23:36 -0800

    Updated unittests to have a separate session class.

--- tests.py
+++ tests.py
@@ -10,7 +10,7 @@ geocodekey = os.environ['geocodekey']
 
 
 class ParkTests(unittest.TestCase):
-    """Testing suite for server.py."""
+    """Testa for Parktake app for functions that don't require sessions."""
 
     def setUp(self):
         # set up fake test browser
@@ -22,19 +22,20 @@ class ParkTests(unittest.TestCase):
         # This line makes a 500 error in a route raise an error in a test
         app.config['TESTING'] = True
 
-        # # create tables and add sample data
+        # create tables and add sample data
         db.create_all()
         example_data_rec_areas()
         example_data_users()
         example_data_visits()
 
-#############################################################################
-# Test any functions that only render a template.
+    #############################################################################
+    # Test any functions that only render a template.
 
     def test_load_homepage(self):
         """Tests to see if the index page comes up."""
 
         result = self.client.get('/')
+        # print dir(result) to see what methods are available for result
 
         self.assertEqual(result.status_code, 200)
         self.assertIn('text/html', result.headers['Content-Type'])
@@ -67,19 +68,6 @@ class ParkTests(unittest.TestCase):
         self.assertIn('text/html', result.headers['Content-Type'])
         self.assertIn('Parktake was inspired by a love of adventure.', result.data)
 
-    def test_load_landing(self):
-        """Tests to see if the landing page comes up."""
-
-        with self.client as c:
-            with c.session_transaction() as sess:
-                sess['user'] = '2'
-            c.set_cookie('localhost', 'MYCOOKIE', 'cookie_value')
-            result = c.get('/landing', data={'mapkey': mapkey}, follow_redirects=True)
-
-        self.assertEqual(result.status_code, 200)
-        self.assertIn('text/html', result.headers['Content-Type'])
-        self.assertIn('Your Parks', result.data)
-
     def test_load_logout(self):
         """Tests to see if logout occurs properly."""
 
@@ -89,8 +77,8 @@ class ParkTests(unittest.TestCase):
         self.assertIn('text/html', result.headers['Content-Type'])
         self.assertIn('<a id="nav-login" href="/login">Log In</a>', result.data)
 
-#############################################################################
-# Test any functions that will query data or require a session.
+    #############################################################################
+    # Test any functions that will query data.
 
     def test_process_signup_new_user(self):
         """Test to see if the signup form will process new user properly."""
@@ -117,6 +105,7 @@ class ParkTests(unittest.TestCase):
                                   follow_redirects=True)
         self.assertIn('/login', result.data)
         self.assertNotIn('Welcome, ', result.data)
+        self.assertIn('You already have an account. Please login', result.data)
 
     def test_process_login_known(self):
         """Test to see if the login form will process properly with a known user."""
@@ -151,18 +140,7 @@ class ParkTests(unittest.TestCase):
         self.assertIn('Log In', result.data)
         self.assertIn('That email and password combination does not exist.', result.data)
 
-    def test_show_account_logged_in(self):
-        """Test to see if account page will show up if a user is logged in."""
 
-        with self.client as c:
-            with c.session_transaction() as sess:
-                sess['user'] = '2'
-            c.set_cookie('localhost', 'MYCOOKIE', 'cookie_value')
-            result = c.get('/account')
-
-        self.assertIn('Your Account', result.data)
-        self.assertIn('Lucy', result.data)
-        self.assertNotIn('/login', result.data)
 
     def test_show_account_not_logged_in(self):
         """Test to see if account page will show up if a user isn't logged in."""
@@ -171,37 +149,6 @@ class ParkTests(unittest.TestCase):
 
         self.assertNotIn('Your Account', result.data)
         self.assertIn('/login', result.data)
-
-    def test_update_account_logged_in(self):
-        """Test to see if update account page will show up if a user is logged in."""
-
-        with self.client as c:
-            with c.session_transaction() as sess:
-                sess['user'] = '2'
-            c.set_cookie('localhost', 'MYCOOKIE', 'cookie_value')
-            result = c.get('/update-account')
-
-        self.assertIn('Update Your Account', result.data)
-        self.assertIn('Lucy', result.data)
-        self.assertNotIn('/login', result.data)
-
-    def test_save_account_logged_in(self):
-        """Test to see if save account page will show up if a user is logged in."""
-
-        with self.client as c:
-            with c.session_transaction() as sess:
-                sess['user'] = '2'
-            c.set_cookie('localhost', 'MYCOOKIE', 'cookie_value')
-            result = c.post('/save-changes', data={'first_name': 'Lucy',
-                                                   'last_name': 'Vo',
-                                                   'email': 'lucy@test.com',
-                                                   'password': 'squirrel',
-                                                   'zipcode': '94306'},
-                                             follow_redirects=True)
-
-        self.assertIn('Your account has been updated.', result.data)
-        self.assertIn('Lucy', result.data)
-        self.assertNotIn('/login', result.data)
 
     def test_save_account_not_logged_in(self):
         """Test to see if save account page will show up if a user is not logged in."""
@@ -227,27 +174,126 @@ class ParkTests(unittest.TestCase):
 
         self.assertIsInstance(get_parks(visited_parks), dict)
 
-    def test_parks_json(self):
-        """Test to see if /parks.json route will return json response."""
+class ParkTestsSession(unittest.TestCase):
+    """Testa for Parktake app for functions that don't require sessions."""
 
+    def setUp(self):
+        # set up fake test browser
+        self.client = app.test_client()
+
+        # connect to temporary database
+        connect_to_db(app, "sqlite:///")
+
+        # This line makes a 500 error in a route raise an error in a test
+        app.config['TESTING'] = True
+
+        # create tables and add sample data
+        db.create_all()
+        example_data_rec_areas()
+        example_data_users()
+        example_data_visits()
+
+        # initiate a session
         with self.client as c:
-            with c.session_transaction() as sess:
-                sess['user'] = '2'
-            c.set_cookie('localhost', 'MYCOOKIE', 'cookie_value')
-            response = c.get("/parks.json")
+                with c.session_transaction() as sess:
+                    sess['user'] = '2'
+                c.set_cookie('localhost', 'MYCOOKIE', 'cookie_value')
+
+    #############################################################################
+    # Test any functions that only render a template.
+
+    def test_load_landing(self):
+        """Tests to see if the landing page comes up."""
+
+        result = self.client.get('/landing', data={'mapkey': mapkey}, follow_redirects=True)
+
+        self.assertEqual(result.status_code, 200)
+        self.assertIn('text/html', result.headers['Content-Type'])
+        self.assertIn('Your Parks', result.data)
+
+    #############################################################################
+    # Test any functions that will query data.
+
+    def test_process_login_already_logged(self):
+        """Test to see if the login form will show if user is already logged in."""
+
+        result = self.client.get('/login')
+
+        self.assertIn('You are already logged in', result.data)
+
+    def test_show_account_logged_in(self):
+        """Test to see if account page will show up if a user is logged in."""
+
+        result = self.client.get('/account')
+
+        self.assertIn('Your Account', result.data)
+        self.assertIn('Lucy', result.data)
+        self.assertNotIn('/login', result.data)
+
+    def test_update_account_logged_in(self):
+        """Test to see if update account page will show up if a user is logged in."""
+
+        result = self.client.get('/update-account')
+
+        self.assertIn('Update Your Account', result.data)
+        self.assertIn('Lucy', result.data)
+        self.assertNotIn('/login', result.data)
+
+    def test_save_account_logged_in(self):
+        """Test to see if save account page will show up if a user is logged in."""
+
+        result = self.client.post('/save-changes', data={'first_name': 'Lucy',
+                                                   'last_name': 'Vo',
+                                                   'email': 'lucy@test.com',
+                                                   'password': 'squirrel',
+                                                   'zipcode': '94306'},
+                                             follow_redirects=True)
+
+        self.assertIn('Your account has been updated.', result.data)
+        self.assertIn('Lucy', result.data)
+        self.assertNotIn('/login', result.data)
+
+    def test_view_park(self):
+        """Test to see if user can view their parks if a user is logged in."""
+
+        result = self.client.get('/view-park')
+
+        self.assertIn('Based on where you\'ve been,', result.data)
+
+    #############################################################################
+    # Test any functions to see if they're an instance of a built-in class
+
+    def test_parks_json(self):
+        """Test to see if /parks.json route will return json object."""
+
+        response = self.client.get("/parks.json")
 
         self.assertIsInstance(response, object)
 
     def test_visited_parks_json(self):
-        """Test to see if /parks-visited.json route will return json response."""
+        """Test to see if /parks-visited.json route will return json object."""
 
-        with self.client as c:
-            with c.session_transaction() as sess:
-                sess['user'] = '2'
-            c.set_cookie('localhost', 'MYCOOKIE', 'cookie_value')
-            response = c.get("/parks-visited.json")
+        response = self.client.get("/parks-visited.json")
 
         self.assertIsInstance(response, object)
+
+    def test_get_chart_data(self):
+        """Test to see if /parks-visited.json route will return json object."""
+
+        response = self.client.get("/parks-in-states.json")
+
+        self.assertIsInstance(response, object)
+
+    #############################################################################
+    # Test suggestion feature
+
+    def test_suggestion_feature(self):
+        """Test to see if the proper park suggestion is offered."""
+
+        response = self.client.get("/suggest-park")
+
+        self.assertIn('Zion National Park', response.data)
+
 
 if __name__ == "__main__":
     unittest.main()
