d140ca7c09454516771356bc5b707a4e4bbd5a92
Jackie Huynh <jackiehuynh@Jackies-MacBook-Pro.local>
2015-12-10 19:21:53 -0800

    Updated server file

--- server.py
+++ server.py
@@ -531,7 +531,7 @@ def check_password(hashed_password, user_password):
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the point
     # that we invoke the DebugToolbarExtension
-    app.debug = True
+    # app.debug = True
 
     connect_to_db(app)
     # connect_to_db(app, os.environ.get("DATABASE_URL"))
